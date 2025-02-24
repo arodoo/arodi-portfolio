b4a5ffbf1c1e5929f43b5f81e553017c8b86fd90
arodoo <haro.em@hotmail.com>
2025-02-23 21:52:07 -0600

    refactor ThreeSharkService for improved model loading; enhance About component layout and styling

--- src/app/features/about/components/about/about.component.ts
+++ src/app/features/about/components/about/about.component.ts
@@ -1,20 +1,29 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { ThreeEarthService } from '../../../../core/services/three-js/three-earth.service';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent implements AfterViewInit {
-  @ViewChild('earthCanvas') canvasRef!: ElementRef<HTMLCanvasElement>;
-
-  constructor(private threeEarthService: ThreeEarthService) {}
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.threeEarthService.init(this.canvasRef);
-    }, 100);
-  }
+export class AboutComponent {
+  team = [
+    {
+      name: 'Editorial Team',
+      description: 'Curating and reviewing poetry submissions',
+      icon: 'fa-edit'
+    },
+    {
+      name: 'Community Team',
+      description: 'Building and nurturing our poetry community',
+      icon: 'fa-users'
+    },
+    {
+      name: 'Technical Team',
+      description: 'Maintaining and improving the platform',
+      icon: 'fa-cog'
+    }
+  ];
 }
