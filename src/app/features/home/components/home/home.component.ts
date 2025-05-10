259a3ab8a3383b48c6cea8d2834cfc8c79d6abf1
arodoo <haro.em@hotmail.com>
2025-05-10 10:51:33 -0600

    feat(Contact): add Contact component with template and styles; integrate into Home component refactor(Home): include Contact component in Home layout; enhance structure refactor(Services): clean up Services component; remove contact section

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -5,24 +5,26 @@ import { FooterComponent } from '../../../../shared/components/footer/footer.com
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
 import { AboutComponent } from '../../../about/components/about/about.component';
 import { ServicesComponent } from '../../../services/components/services/services.component';
+import { ContactComponent } from '../../../contact/components/contact/contact.component';
 
 //loading-bar
 import { LoadingService } from '../../../../core/services/loading.service';
 import { LoadingBarComponent } from '../../../../shared/animations/loading-bar/loading-bar.component';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    imports: [
-        LoadingBarComponent,
-        HeaderComponent, 
-        FooterComponent, 
-        NavbarComponent,
-        AboutComponent,
-        ServicesComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    LoadingBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent implements AfterViewInit {
 
